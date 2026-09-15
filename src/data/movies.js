c0908dcefd8ab0b5dd50381034c7b8d896b2135f
mariahbargiona <mariahbargiona@gmail.com>
2026-09-15 10:18:17 -0300

    configurando homepage e catálagos

---
+++ src/data/movies.js
@@ -0,0 +1,52 @@
+export const movies = [
+  {
+    id: 1,
+    title: "Código Sombrio",
+    description: "Um jovem programador descobre uma falha perigosa em um sistema global.",
+    type: "filme",
+    category: "Lançamentos",
+    coverImage: "https://images.unsplash.com/photo-1489599849927-2ee91cede3ba",
+    bannerImage: "https://images.unsplash.com/photo-1524985069026-dd778a71c7b4",
+    featured: true
+  },
+  {
+    id: 2,
+    title: "Terminal Zero",
+    description: "Uma equipe precisa impedir um ataque digital antes da meia-noite.",
+    type: "filme",
+    category: "Ação",
+    coverImage: "https://images.unsplash.com/photo-1536440136628-849c177e76a1",
+    bannerImage: "https://images.unsplash.com/photo-1524985069026-dd778a71c7b4",
+    featured: false
+  },
+  {
+    id: 3,
+    title: "Dados Ocultos",
+    description: "Um documentário sobre o poder dos dados na sociedade moderna.",
+    type: "documentario",
+    category: "Documentários",
+    coverImage: "https://images.unsplash.com/photo-1516321318423-f06f85e504b3",
+    bannerImage: "https://images.unsplash.com/photo-1497366754035-f200968a6e72",
+    featured: false
+  },
+  {
+    id: 4,
+    title: "Bug Infinito",
+    description: "Uma série de comédia sobre uma equipe de desenvolvedores iniciantes.",
+    type: "serie",
+    category: "Séries",
+    coverImage: "https://images.unsplash.com/photo-1515879218367-8466d910aaa4",
+    bannerImage: "https://images.unsplash.com/photo-1519389950473-47ba0277781c",
+    featured: false
+  },
+  {
+    id: 5,
+    title: "Shrek",
+    description: "Um terrível ogro embarca em uma aventura para resgatar uma princesa.",
+    type: "filme",
+    category: "Ação",
+    coverImage: "https://images.unsplash.com/photo-1723988965218-2eabb691f9dc?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    bannerImage: "https://images.unsplash.com/photo-1624720564142-04c1c577ba7f?q=80&w=1160&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    featured: false
+  }
+];
